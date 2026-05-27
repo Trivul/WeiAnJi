8b48ae49c1f1f6527e018c3edabb93b65e8945ac
Yi Liu <1733665084@qq.com>
2026-05-27 15:40:24 +0800

    Initial Commit

---
+++ 胃安记/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  胃安记
+//
+//  Created by Yi Liu on 5/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
